b116f0b4e31cab278f902f021d55a7dbc5756116
Milgam06 <milgamfruit@gmail.com>
2022-11-15 21:31:39 +0900

    first commit

---
+++ roulette.py
@@ -0,0 +1,46 @@
+# module load
+import random  
+import os
+import platform
+from time import sleep   
+
+
+
+menu = ""
+menus = []
+
+def input_menu():   # 후보기입 함수
+    global menus
+    global menu
+    hubo = int(input("후보 갯수 >>>"))  
+    for i in range(1, hubo+1):
+        menu = input("먹고싶은 메뉴 %d번째 후보를 적어주세요! - " %i)
+        menus.append(menu)
+    return menus, menu
+
+def chDinner():     # 메뉴추첨 함수
+    global menus
+    sleep(1)
+    print("\n당신이 고른 후보들 중 하나를 추첨해 드릴게요!")
+    for f in range(4):
+        sleep(0.5)
+        print(".", end="")
+    result = random.choice(menus)
+    print(f"\n{result}'가 나왔네요! 5초 뒤에 배달사이트 열어드릴게요!")
+    sleep(5)
+    try:        # 시스템 운영체제 확인 & 배달사이트 오픈
+        if platform.system() == "Windows":
+            os.system('explorer "https://www.yogiyo.co.kr/mobile/#/"')
+            return
+        elif platform.system() == "Darwin":
+            os.system('open "https://www.yogiyo.co.kr/mobile/#/"')
+            return
+    except:
+        print("당신이 뭔가 일을 저질렀어요! 자숙하세요!")
+        exit()
+
+
+# 실행
+input_menu()
+chDinner()
+    
